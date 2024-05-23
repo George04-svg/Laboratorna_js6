391fa371a7aa21356ae6aef402c1285e84463f25
George K. <4058128@gmail.com>
2024-05-23 16:09:37 +0300

    first commit

---
+++ styles.js
@@ -0,0 +1,13 @@
+function filterBy(array, typeOfData) {
+    const filtered = [];
+    for (let i = 0; i < array.length; i++) {
+      if (typeof array[i] !== typeOfData) {
+        filtered.push(array[i]);
+      }
+    }
+    return filtered;
+  }
+  
+  const example = ['hello', 'world', 23, '23', null];
+  
+  console.log(filterBy(example, Boolean));
